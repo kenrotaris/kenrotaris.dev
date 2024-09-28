7f05ba11b73f84587007715ff9e4f78b5748b7fd
Ken Rotaris <contact@kenrotaris.dev>
2024-09-29 00:20:01 +0200

    add footer module

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <a href="/">Home</a>
+      <a href="https://www.linkedin.com/in/kenrotaris/">LinkedIn</a>
+      <a href="mailto:info@kenrotaris.dev">Email</a>
+    </div>
+  );
+}
+
+export default Footer;

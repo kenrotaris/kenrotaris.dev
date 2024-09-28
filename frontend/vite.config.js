a15e786593d1ebfdc77ad8b4cd535a20435888a4
Ken Rotaris <contact@kenrotaris.dev>
2024-09-29 00:01:32 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [
-      react()
-    ],
-
+    plugins: [react()],
     server: mode === 'development' ? {
       port: 7778,
       proxy: { // because locally we dont use nginx we need to add a proxy which bypasses the cross origin by making requests appear to come from the same origin (localhost:7778), because the frontend and backend run on different ports.
@@ -18,7 +15,7 @@ export default defineConfig(({ mode }) => {
         }
       }
     } : undefined, // In production, no proxy is used because NGINX handles the reversy proxying from kenrotaris.dev to the proper ports so from the browsers perspective its the same origin
-
+    logLevel: 'info',
     build: {
       sourcemap: mode === 'development', // Vite should generate source maps during the build process (mapping the minified or transpiled code back to the original source code)
     },

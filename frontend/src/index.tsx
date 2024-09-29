615a7fe4da4b4e1673f7506551c137f9aea2ba5b
Ken Rotaris <contact@kenrotaris.dev>
2024-09-29 23:14:31 +0200

    rename

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
+// Entry point of the application
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 
 const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
